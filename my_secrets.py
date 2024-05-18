f712020f6825046923e3f835c5508a7b3f1d6607
Vaibhav Dixit <vaibhav84334@gmail.com>
2024-05-18 17:13:48 +0530

    created my_secrets.py

---
+++ my_secrets.py
@@ -0,0 +1,45 @@
+OPENAI_API_KEY = "omg fill it bro"
+
+GOOGLE_DOC_ID = "ok now this"
+my_json_file = 'hehe this too'
+
+name = "Vaibhav Dixit"
+address = "New Delhi, India"
+phone = "+91-8433470760"
+email = "vaibhav_d@ch.iitr.ac.in"
+education = "BTech from Indian Institute of Technology(IIT) Roorkee"
+work_experience = """
+Software Engineer Intern (Back-end) February 2024 - Present
+Uniacco Mumbai, Maharashtra
+• Coded the entire workflow and cronjob for migrating 2,00,000+ mp3 files from knowlarity server to AWS S3.
+• Optimized PDF compression algorithm resulting in more than 95% use of target size & lesser quality decrement.
+• Utilized OpenAI's content creation techniques to optimize content generation, resulting in higher SEO rankings.
+
+Software Engineer Intern October 2023 - December 2023
+Wozo Gaming New Delhi
+• Fixed high-priority bugs to handle incidents related to user badge assignment, along with updating dependencies.
+• Developed an end to end automated Cloud SQL migration, created pipeline to deploy in development & production
+environments, leveraged CI/CD workflows via GitHub Actions with cloud SQL proxy & Goose.
+• Investigated deprecated v1 endpoints with usage less than 2.5k users with DataDog while shifting to v2.
+
+Software Developer Intern July 2023 - September 2023
+Career A Plus Bangalore, Karnataka
+• Improved the back-end of the resume builder section and career plan section by creating 20+ component logics.
+• Enhanced front-end of the web portal and integrated SCSS for better styling, increasing 27% user engagement.
+• Cleared bugs, discussed approaches & developed key features according to the requirements of the product team.
+
+Research Analyst Intern May 2023 - July 2023
+Bureau Of Indian Standards New Delhi
+• Executed extensive research and data analysis for development of pre-standard on Silicone Rubber specifications.
+• Evaluated the feasibility of IS, ISO, and ASTM standards & adopted 60% silicone testing standards from ASTM.
+• Analyzed technical data from 35+ leading manufacturing organizations and labs collected during site visits.
+"""
+skills = """
+Languages: C/C++, Java, Python, JavaScript, TypeScript, HTML, CSS, R, Golang.
+Frameworks: React.js, Node.js,Express.js, Next.js, Django, Django REST Framework, gRPC, Redis.
+Developer Tools: Git, Docker, Google Cloud Platform, Visual Studio, PyCharm, IntelliJ, Eclipse, Jenkins, AWS S3,
+Kubernetes, Terraform, Spinnaker, DataDog, Sourcegraph, Celery.
+Databases: MariaDB, Spanner, mySQL, CloudSQL, PostgreSQL, MongoDB.
+"""
+salary_expectations = "bhot sara rupees"
+possible_start_date = "Available Immediately"
